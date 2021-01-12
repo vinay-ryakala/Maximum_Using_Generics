83dfc452c9c3a2f7152f28d339f8e638870a208a
vinay-ryakala <vinayrakyala@gmail.com>
2021-01-12 22:05:17 +0530

    ADDED:To get maximum from three integers using compare to method

--- src/main/java/com/genric/Maximum.java
+++ src/main/java/com/genric/Maximum.java
@@ -1,3 +1,13 @@
 package com.genric;
 
 //Welcome to  maximum number using generics
+public class Maximum {
+    public Integer getMax(Integer firstValue, Integer secondValue, Integer thirdValue) {
+        Integer max = firstValue;
+        if(secondValue.compareTo(max) > 0)
+            max = secondValue;
+        if(thirdValue.compareTo(max) > 0)
+            max = thirdValue;
+        return max;
+    }
+}
--- src/test/java/com/genric/MaximumTest.java
+++ src/test/java/com/genric/MaximumTest.java
@@ -1,3 +1,32 @@
 package com.genric;
 
 //Welcome to  maximum number using generics
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MaximumTest {
+    @Test
+    public void givenIntegerFirstNumberGrater_WhenLargest_ShouldReturnMax(){
+        Integer expected = 90;
+        Maximum maximum = new Maximum();
+        Integer acctualResult =  maximum.getMax(90,20,10);
+        Assert.assertEquals(expected,acctualResult);
+
+    }
+    @Test
+    public void givenIntegerSecondNumberGrater_WhenLargest_ShouldReturnMax(){
+        Integer expected = 90;
+        Maximum maximum = new Maximum();
+        Integer acctualResult =  maximum.getMax(20,90,10);
+        Assert.assertEquals(expected,acctualResult);
+    }
+
+    @Test
+    public void givenIntegerThirdNumberGrater_WhenLargest_ShouldReturnMax(){
+        Integer expected = 90;
+        Maximum maximum = new Maximum();
+        Integer acctualResult =  maximum.getMax(20,10,90);
+        Assert.assertEquals(expected,acctualResult);
+
+    }
+}
