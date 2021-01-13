e9399fa2cf34a60da68bc2283699cdbd180cfd53
vinay-ryakala <vinayrakyala@gmail.com>
2021-01-13 10:03:12 +0530

    REFACTOR: to create  generic calss and test case for generic class

--- src/main/java/com/genric/Maximum.java
+++ src/main/java/com/genric/Maximum.java
@@ -1,7 +1,18 @@
 package com.genric;
 
 //Welcome to  maximum number using generics
-public class Maximum {
+public class Maximum<T extends Comparable<T>> {
+    T x,y,z;
+
+    public Maximum(T x,T y,T z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public T maximum(){
+        return Maximum.maximumGenericMethod(x,y,z);
+    }
+
     //using generic method to find maximum
     public static <E extends Comparable<E>> E maximumGenericMethod(E firstValue,E secondValue,E thirdValue){
         E max = firstValue;
--- src/test/java/com/genric/MaximumTest.java
+++ src/test/java/com/genric/MaximumTest.java
@@ -8,8 +8,7 @@ public class MaximumTest {
     @Test
     public void givenIntegerFirstNumberGrater_WhenLargest_ShouldReturnMax(){
         Integer expected = 90;
-        Maximum maximum = new Maximum();
-        Integer acctualResult =  maximum.maximumGenericMethod(90,20,10);
+        Integer acctualResult =  Maximum.maximumGenericMethod(90,20,10);
         Assert.assertEquals(expected,acctualResult);
 
     }
@@ -17,62 +16,63 @@ public class MaximumTest {
     @Test
     public void givenIntegerSecondNumberGrater_WhenLargest_ShouldReturnMax(){
         Integer expected = 90;
-        Maximum maximum = new Maximum();
-        Integer acctualResult =  maximum.maximumGenericMethod(20,90,10);
+        Integer acctualResult =  Maximum.maximumGenericMethod(20,90,10);
         Assert.assertEquals(expected,acctualResult);
     }
 
     @Test
     public void givenIntegerThirdNumberGrater_WhenLargest_ShouldReturnMax(){
         Integer expected = 90;
-        Maximum maximum = new Maximum();
-        Integer acctualResult =  maximum.maximumGenericMethod(20,10,90);
+        Integer acctualResult =  Maximum.maximumGenericMethod(20,10,90);
         Assert.assertEquals(expected,acctualResult);
 
     }
     @Test
     public void givenFloatFirstNumberGrater_WhenLargest_ShouldReturnMax(){
         Float expectedFloat = 90.5f;
-        Maximum maximum = new Maximum();
-        Float acctualResultFloat =  maximum.maximumGenericMethod(90.5f,20.56f,10.52f);
+        Float acctualResultFloat =  Maximum.maximumGenericMethod(90.5f,20.56f,10.52f);
         Assert.assertEquals(expectedFloat,acctualResultFloat);
 
     }
     @Test
     public void givenFloatSecondNumberGrater_WhenLargest_ShouldReturnMax(){
         Float expectedFloat = 90.5f;
-        Maximum maximum = new Maximum();
-        Float acctualResultFloat =  maximum.maximumGenericMethod(20.5f,90.5f,10.6f);
+        Float acctualResultFloat =  Maximum.maximumGenericMethod(20.5f,90.5f,10.6f);
         Assert.assertEquals(expectedFloat,acctualResultFloat);
     }
     @Test
     public void givenFloatThirdNumberGrater_WhenLargest_ShouldReturnMax(){
         Float expectedFloat = 90.5f;
-        Maximum maximum = new Maximum();
-        Float acctualResultFloat =  maximum.maximumGenericMethod(20.5f,10.5f,90.5f);
+        Float acctualResultFloat =  Maximum.maximumGenericMethod(20.5f,10.5f,90.5f);
         Assert.assertEquals(expectedFloat,acctualResultFloat);
     }
     @Test
     public void givenFirstStringGrater_WhenLargest_ShouldReturnMax(){
         String expectedString = "Bat";
-        Maximum maximum = new Maximum();
-        String acctualResult =  maximum.maximumGenericMethod("Bat","Apple","Ant");
+       // Maximum maximum = new Maximum();
+        String acctualResult =  Maximum.maximumGenericMethod("Bat","Apple","Ant");
         Assert.assertEquals(expectedString,acctualResult);
     }
     @Test
     public void givenSecondStringGrater_WhenLargest_ShouldReturnMax(){
         String expectedString = "Bat";
-        Maximum maximum = new Maximum();
-        String acctualResult =  maximum.maximumGenericMethod("Apple","Bat","Ant");
+        String acctualResult =  Maximum.maximumGenericMethod("Apple","Bat","Ant");
         Assert.assertEquals(expectedString,acctualResult);
     }
     @Test
     public void givenThirdStringGrater_WhenLargest_ShouldReturnMax(){
         String expectedString = "Bat";
-        Maximum maximum = new Maximum();
-        String acctualResult =  maximum.maximumGenericMethod("Apple","Ant","Bat");
+        String acctualResult =  Maximum.maximumGenericMethod("Apple","Ant","Bat");
         Assert.assertEquals(expectedString,acctualResult);
     }
-
-
+    //using generic class
+    @Test
+    public void parametricGenricClass() {
+        Integer firstInt=1,secondInt=2,thirdInt=3;
+        Float firstFloat=1.2f,secondFloat=9.8f,thirdFloat=6.5f;
+        String firstString="apple",secondString="ball",thirdString="bat";
+        Assert.assertEquals(new Maximum(firstString,secondString,thirdString).maximum(),"bat");
+        Assert.assertEquals(new Maximum(firstInt,secondInt,thirdInt).maximum(),3);
+        Assert.assertEquals(new Maximum(firstFloat,secondFloat,thirdFloat).maximum(),9.8f);
+   }
 }
